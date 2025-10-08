1c09321709ec020411cd14144a672dec86718250
davinastra777 <davinastra_2327240037@mhs.mdp.ac.id>
2025-10-08 14:39:08 +0700

    luas persegi panjang

--- dart_persegi_panjang/bin/dart_persegi_panjang.dart
+++ dart_persegi_panjang/bin/dart_persegi_panjang.dart
@@ -2,13 +2,11 @@ class PersegiPanjang {
   int panjang;
   int lebar;
 
-<<<<<<< HEAD
-PersegiPanjang(this.panjang,this.lebar); 
-int luasPersegiPanjang() {
-  return panjang * lebar;
-
-}}
-=======
-  PersegiPanjang(this.panjang,this.lebar);
+  PersegiPanjang(this.panjang, this.lebar);
+  int hitungLuas() {
+    return panjang * lebar;
+  }
 }
->>>>>>> db261f6191e9b5fe281ac933deb76cb3a22cdcd0
+
+
+
